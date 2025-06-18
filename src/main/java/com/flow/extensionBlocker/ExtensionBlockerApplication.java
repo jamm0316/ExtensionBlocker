7d53a203d9b34f696a692500bb9ba87c611a6511
jamm0316 <jamm0316@naver.com>
2025-06-18 21:58:56 +0900

    feat: auditing, auditorAwareImpl 추가
    1. auditingListener추가 -> createAt, modifiedAt 자동추가
    2. AuditorAwareImpl 추가 -> createBy, modifiedBy 자동추가
    
    Resolves #8

--- src/main/java/com/flow/extensionBlocker/ExtensionBlockerApplication.java
+++ src/main/java/com/flow/extensionBlocker/ExtensionBlockerApplication.java
@@ -2,12 +2,13 @@ package com.flow.extensionBlocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ExtensionBlockerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExtensionBlockerApplication.class, args);
 	}
-
 }
--- src/main/java/com/flow/extensionBlocker/common/BaseTimeEntity.java
+++ src/main/java/com/flow/extensionBlocker/common/BaseTimeEntity.java
@@ -1,16 +1,19 @@
 package com.flow.extensionBlocker.common;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 public class BaseTimeEntity {
     @CreatedDate
