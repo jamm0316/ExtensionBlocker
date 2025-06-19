ed03be467d47c63ee61a68a09d7c86fa2e93c7d6
jamm0316 <jamm0316@naver.com>
2025-06-19 11:12:11 +0900

    feat: customExtension 전체 찾기 API 만들기
    1. 직접 DTO로 매핑하는 쿼리 작성
    
    Resolves #20

--- src/main/java/com/flow/extensionBlocker/repository/ExtensionBlockerRepository.java
+++ src/main/java/com/flow/extensionBlocker/repository/ExtensionBlockerRepository.java
@@ -1,12 +1,26 @@
 package com.flow.extensionBlocker.repository;
 
 import com.flow.extensionBlocker.domain.ExtensionBlocker;
+import com.flow.extensionBlocker.dto.ExtensionBlockerResponseDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ExtensionBlockerRepository extends JpaRepository<ExtensionBlocker, Long> {
     ExtensionBlocker findByName(String name);
+
+    @Query("""
+            select new com.flow.extensionBlocker.dto.ExtensionBlockerResponseDTO (
+                  e.name, e.type, e.isBanned          
+                )
+            from ExtensionBlocker e
+            where e.type = com.flow.extensionBlocker.domain.ExtensionType.CUSTOM
+                and e.isBanned = true
+            """)
+    List<ExtensionBlockerResponseDTO> findAllCustomExtension();
 
     long countByIsBannedTrue();
 }
