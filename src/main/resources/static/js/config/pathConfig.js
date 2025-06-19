f8af5c8b4f0c7079cf432fcd479e584d10217b8b
jamm0316 <jamm0316@naver.com>
2025-06-19 13:40:28 +0900

    feat: 커스텀 확장자 등록 버튼 연결
    1. index > ui > service > api 순으로 흐름에 따라 모듈 분리
    2. 커스텀 확장자 등록 버튼 API연결
    3. 테스트 완료
    
    Resolves #23

---
+++ src/main/resources/static/js/config/pathConfig.js
@@ -0,0 +1,3 @@
+export const CUSTOM_EXT_ROOT_PATH = '/static/js/extension/custom-extension'
+export const FIXED_EXT_ROOT_PATH = '/static/js/extension/fixed-extension'
+export const API_PATH = '/api/extension'
---
+++ src/main/resources/static/js/extension/custom-extension/index.js
@@ -0,0 +1,5 @@
+import {initCustomExtensionUI} from "/js/extension/custom-extension/ui.js";
+
+$(document).ready(function () {
+  initCustomExtensionUI();
+});
