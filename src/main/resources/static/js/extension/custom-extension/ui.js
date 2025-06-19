11165ddc02088b8f2c9f03f0a909324980562601
jamm0316 <jamm0316@naver.com>
2025-06-19 16:39:43 +0900

    feat: 커스텀 확장자 삭제 기능 연동
    1. th:each의 반복요소에서 각 div를 제어하기 위해 data-name 추가
    2. api 호출 및, ui 버튼 연결 구현
    
    Resolves #27

--- src/main/resources/static/js/extension/custom-extension/ui.js
+++ src/main/resources/static/js/extension/custom-extension/ui.js
@@ -1,19 +1,30 @@
-import {addCustomExtension} from "/js/extension/custom-extension/api.js";
+import {addCustomExtension, deleteCustomExtension} from "/js/extension/custom-extension/api.js";
 import {sanitize, validateExtension} from "/js/extension/custom-extension/service.js";
+
+function validationAndAlert(ext, count) {
+  const {valid, message} = validateExtension(ext, count);
+  if (!valid) alert(message);
+  return valid;
+}
 
 export function initCustomExtensionUI() {
   $("#custom-add-btn").on("click", () => {
-    const ext = sanitize($("#custom-input").val());
-    const currentCount = $("#custom-list .badge").length;
+    let ext = sanitize($("#custom-input").val());
+    let currentCount = $("#custom-list .badge").length;
 
-    const {valid, message} = validateExtension(ext, currentCount);
-    if (!valid) {
-      alert(message);
-      return;
-    }
+    if (!validationAndAlert(ext, currentCount)) return;
 
     addCustomExtension(ext)
       .then(() => location.reload())
       .catch(() => alert("추가에 실패했습니다."));
+  })
+
+  $(".remove-btn").on("click", function () {
+    let ext = sanitize($(this).data("name"));
+    if (!validationAndAlert(ext)) return;
+
+    deleteCustomExtension(ext)
+      .then(() => location.reload())
+      .catch(() => alert("삭제에 실패했습니다."))
   })
 }
