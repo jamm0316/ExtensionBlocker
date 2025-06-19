0711d1d81ee8ad21136c2a72a28f9695b3ffdfb3
jamm0316 <jamm0316@naver.com>
2025-06-19 11:16:35 +0900

    design: 화면 기본 틀 만들기
    1. 확장자 차단 화면 기본 틀 만들기
    
    Resolves #17

--- src/main/java/com/flow/extensionBlocker/controller/view/ExtensionBlockerViewController.java
+++ src/main/java/com/flow/extensionBlocker/controller/view/ExtensionBlockerViewController.java
@@ -1,14 +1,23 @@
 package com.flow.extensionBlocker.controller.view;
 
+import com.flow.extensionBlocker.dto.ExtensionBlockerResponseDTO;
+import com.flow.extensionBlocker.service.ExtensionBlockerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ExtensionBlockerViewController {
-    @GetMapping("hello")
+    @Autowired
+    ExtensionBlockerService service;
+
+    @GetMapping("extension/blocker")
     public String hello(Model model) {
-        model.addAttribute("name", "evan");
-        return "hello";
+        List<ExtensionBlockerResponseDTO> extensionList = service.selectAllCustomExtension();
+        model.addAttribute("extensionList", "extensionList");
+        return "index";
     }
 }
