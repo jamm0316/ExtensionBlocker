5ce701cd75c365d31976326683edb3a9ab205e8d
jamm0316 <jamm0316@naver.com>
2025-06-20 14:05:27 +0900

    refactor: utils 유효성 검사 수정
    1. customExtensionUI에서 utils.validationAlert() 사용
    2. 영소문자 + 숫자 허용 정규식 추가
    3. 커스텀 확장자 입력에서 고정 확장자 입력 시 해당 확장자로 포커싱
    4. 커스텀 확장자 입력에서 이미 입력된 확장자 입력 시 해당 에러 메세지 표시
    
    Resolves #66

--- src/main/resources/static/js/extension/custom-extension/customExtensionUI.js
+++ src/main/resources/static/js/extension/custom-extension/customExtensionUI.js
@@ -7,6 +7,8 @@ export function initCustomExtensionUI() {
     let currentCount = $("#custom-list .badge").length;
 
     if (!validationAndAlert(ext, currentCount)) return;
+    if (!isCustomExtensionDuplicate(ext)) return alert("이미 추가된 확장자입니다.");
+    if (focusIfFixedExtension(ext)) return alert("고정 확장자 입니다.");
 
     addCustomExtension(ext)
       .then(() => location.reload())
@@ -21,4 +23,31 @@ export function initCustomExtensionUI() {
       .then(() => location.reload())
       .catch(() => alert("삭제에 실패했습니다."))
   })
+}
+
+function isCustomExtensionDuplicate(ext) {
+  const existingCustomExt = new Set(
+    $('.blocked-custom-extension-name').map(function () {
+      return $(this).text().toLowerCase();
+    }).get()
+  );
+
+  return !existingCustomExt.has(ext);
+}
+
+function focusIfFixedExtension(ext) {
+  const existingFixedExt = new Set(
+    $('.form-check-input').map(function () {
+      return sanitize($(this).data("name"));
+    }).get()
+  );
+
+  if (existingFixedExt.has(ext)) {
+    $('.form-check-input').each(function () {
+      if (sanitize($(this).data("name")) === ext) {
+        $(this).focus();
+      }
+    });
+    return existingFixedExt.has(ext);
+  }
 }
