c27d85e9eb83129442dd89ab362287dc65416a78
jamm0316 <jamm0316@naver.com>
2025-06-20 16:31:47 +0900

    refactor: 커스텀 확장자 추가/제거 시 페이지 랜더링 방식으로 바꾸기
    페이지 새로고침 방식 -> 페이지 랜더링 방식(컴포넌트 추가/제거 방식)
    
    Resolves #71

--- src/main/resources/static/js/extension/common/utils.js
+++ src/main/resources/static/js/extension/common/utils.js
@@ -6,7 +6,7 @@ export function validateExtension(ext, count) {
   let extLowerCase = sanitize(ext);
 
   if (!extLowerCase || extLowerCase.length === 0) return { valid:false, message: "확장자 명을 입력하세요." }
-  if (!/^[a-z0-9]+$/.test(extLowerCase)) return { valid: false, message: "확장자는 영어 소문자만 입력 가능합니다." }
+  if (!/^[a-z0-9]+$/.test(extLowerCase)) return { valid: false, message: "확장자는 영어 소문자와 숫자만 입력 가능합니다." }
   if (ext.length > 20) return { valid: false, message: "확장자 명은 20자 이내로 작성해주세요." }
   if (count >= 200) return { valid:false, message: "최대 200개까지 등록할 수 있습니다." }
   return {valid: true}
--- src/main/resources/static/js/extension/custom-extension/customExtensionUI.js
+++ src/main/resources/static/js/extension/custom-extension/customExtensionUI.js
@@ -2,7 +2,7 @@ import {addCustomExtension, deleteCustomExtension} from "/js/extension/custom-ex
 import {sanitize, validationAndAlert} from "/js/extension/common/utils.js";
 
 export function initCustomExtensionUI() {
-  $("#custom-add-btn").on("click", () => {
+  $(document).on("click", "#custom-add-btn", () => {
     let ext = sanitize($("#custom-input").val());
     let currentCount = $("#custom-list .badge").length;
 
@@ -11,16 +11,26 @@ export function initCustomExtensionUI() {
     if (focusIfFixedExtension(ext)) return alert("고정 확장자 입니다.");
 
     addCustomExtension(ext)
-      .then(() => location.reload())
+      .then(() => {
+        $("#custom-list").append(customChip(ext));
+        $("#custom-input").val("");
+
+        let currentCount = parseInt($("#custom-count").text(), 10);
+        $("#custom-count").text(currentCount + 1);
+      })
       .catch(() => alert("추가에 실패했습니다."));
   })
 
-  $(".remove-btn").on("click", function () {
+  $(document).on("click", ".remove-btn", function () {
     let ext = sanitize($(this).data("name"));
     if (!validationAndAlert(ext)) return;
 
     deleteCustomExtension(ext)
-      .then(() => location.reload())
+      .then(() => {
+        $(this).closest(".badge").remove();
+        const currentCount = parseInt($("#custom-count").text(), 10);
+        $("#custom-count").text(currentCount - 1);
+      })
       .catch(() => alert("삭제에 실패했습니다."))
   })
 }
@@ -51,4 +61,18 @@ function focusIfFixedExtension(ext) {
     });
     return existingFixedExt.has(ext);
   }
+}
+
+function customChip(extName) {
+  return $(`
+  <div>
+    <div class="badge bg-secondary rounded-pill px-3 py-2" id="chip-list">
+      <span class="blocked-custom-extension-name">${extName}</span>
+      <button type="button"
+              class="btn-close btn-close-white ms-2 remove-btn"
+              data-name="${extName}"
+              aria-label="Remove"></button>
+    </div>
+  </div>
+    `);
 }
